0c50a2e824976cae89618cdb86e379c4cfe40f78
stoptacoman <walsh.dc@gmail.com>
2018-04-09 16:47:11 -0400

    Added basic content to the app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <h3>Boilerplate tings!</h3>
+        <ol>
+          <li>Update title and metadata in index.html</li>
+          <li>Drink some water</li>
+          <li>Close your eyes and try not to think about anything</li>
+          <li>Start coding!</li>
+        </ol>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
